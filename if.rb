8fccd55959f29107ecdf3fe07655e9d2b2ea3908
ababup1192 <ababup1192@gmail.com>
2016-04-28 19:36:32 +0900

    Solve first problem

---
+++ if.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+if true then
+  puts "true is true"
+end
+
+if false then
+  puts "false is false"
+end
+
+if 1<2 then
+  puts "aaaaaaa"
+end
+
+if 1==2 then
+  puts "bbbbbbb"
+end
+
+
+if 1==2 then
+  puts "ccccc"
+else
+  puts "ddddd"
+end
+
+x = 2
+
+if x > 100 then
+  puts "x is 100 over"
+elsif x % 2 == 0 then
+  puts "x is even"
+else
+  puts "x is odd"
+end
